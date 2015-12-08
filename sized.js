3a077520f2d2cc3c951a1425e2bd5b745386031a
Aman Kohli <aman.kohli@citi.com>
2015-12-08 21:37:55 +0000

    added tests; tidied implementation

--- sized.js
+++ sized.js
@@ -6,50 +6,49 @@
 // TODO 20151204 HTTPS
 // TODO 20151204 Multipart support
 //
-var express = require('express')
-var app = express()
+var express = require('express'),
+  app = express(),
+  _ = require('lodash')
 
 var listen_port_http = 8001
 var listen_port_https = 9001
 
-// specification for creating the buffer hashtable, there must be
-// a better way
-//
-var buf_spec = [
-    { key: '5k',
-      value: (5 * 1024)},
-    { key: '10k',
-      value: (10 * 1024)},
-    { key: '20k',
-      value: (20 * 1024)},
-    { key: '30k',
-      value: (30 * 1024)},
-    { key: '40k',
-      value: (40 * 1024)},
-    { key: '50k',
-      value: (50 * 1024)},
-    { key: '100k',
-      value: (100 * 1024)},
-    { key: '200k',
-      value: (200 * 1024)},
-    { key: '300k',
-      value: (300 * 1024)},
-    { key: '400k',
-      value: (400 * 1024)},
-    { key: '500k',
-      value: (500 * 1024)},
-    { key: '1mb',
-      value: (1024 * 1024)}
-]
+var buf_spec = ['5KiB', '10KiB', '20KiB', '30KiB', '40KiB', '50KiB', '100KiB', '200KiB',
+        '300Kib', '400KiB', '500KiB', '1MiB']
 
-var buffers = {}
+var buffers = _.zipObject(buf_spec, _.map(buf_spec, iec_buffer))
 
-buf_spec.forEach(function (spec) {
-  buffers[spec.key] = new Buffer(spec.value)
-  buffers[spec.key].fill(0)
-})
+function iec_buffer (sz_in_a_string) {
+  return new Buffer(iec_bytes(sz_in_a_string))
+}
 
-app.get('/sized/:sz', function (request, response) {
+function iec_bytes (sz) {
+  // known bug, if an unaccepted unit is specified, the numeral
+  // value of bytes is returned
+  // [0] full string; [1] numeral; [2] unit
+  // TODO if (sz.IsInteger) return sz
+  var multiplier = 1
+  var compartmentalised = /([0-9]+)\s*(MiB|KiB){0,1}/.exec(sz)
+
+  if (compartmentalised === null) {
+    return 0
+  }
+
+  switch (compartmentalised[2]) {
+    case 'MiB':
+      multiplier = 1024 * 1024
+      break
+    case 'KiB':
+      multiplier = 1024
+      break
+    default:
+      multiplier = 1
+  }
+
+  return compartmentalised[1] * multiplier
+}
+
+function process_size (request, response) {
   console.log('request made for: ' + request.params.sz)
 
   if (buffers[request.params.sz] !== undefined) {
@@ -58,7 +57,15 @@ app.get('/sized/:sz', function (request, response) {
   } else {
     response.status(404).send('Not Found size: ' + request.params.sz)
   }
-})
+}
+
+module.exports.iec_bytes = iec_bytes
+module.exports.iec_buffer = iec_buffer
+module.exports.process_size = process_size
+
+// TODO - protect via a command line switch
+//
+app.get('/sized/:sz', process_size)
 
 app.listen(listen_port_http)
 console.log('Now listening port ' + listen_port_http)
---
+++ test/sized.js
@@ -0,0 +1,48 @@
+var should = require('chai').should(),
+  _ = require("lodash"),
+  iec_bytes = require("../sized").iec_bytes,
+  iec_buffer = require("../sized").iec_buffer,
+  sized = require("../sized")
+
+  // iec_bytes = require('../sized').iec_bytes
+  // iec_buffer = require('../sized').iec_bytes
+
+describe('no modifier', function() {
+  it ('sending nothing', function() {
+    iec_bytes('').should.equal(0)
+  })
+
+  it ('number without modifier', function() {
+    iec_bytes('15').should.equal(15)
+  })
+
+})
+
+describe('modifier', function() {
+  it ('number with MiB modifier', function() {
+    iec_bytes('12MiB').should.equal(12*1024*1024)
+    iec_bytes('9 MiB').should.equal(9*1024*1024)
+    
+  })
+
+  it ('number with KiB modifier', function() {
+    iec_bytes('101KiB').should.equal(101*1024)
+    iec_bytes('56 KiB').should.equal(56*1024)
+  })
+
+  it ('number with incorrect modifier', function() {
+    // known bug -- cant get my regex to work right now, this
+    // should return 0  but it will return the numeral
+    //
+    iec_bytes('7MB').should.equal(7)
+  })
+})
+
+describe('sized buffer returned', function() {
+  it ('buffer size check', function() {
+//    var buf = iec_buffer('51KiB')
+    iec_buffer('51KiB').length.should.equal(51*1024)
+//    iecnv.iec_buffer('5 MiB').length.should.equal(5*1024*1024)
+  })
+  
+})
